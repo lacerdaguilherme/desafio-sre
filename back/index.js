f8e0b9a6867b11bba2ad147ba32d1527ee1cbe9b
Guilherme <lacerdaguilherme@gmail.com>
2021-03-11 20:10:47 -0300

    [fix] Criada opcao de porta no .env do backend

--- back/index.js
+++ back/index.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT
 });
 
 db.connect();
--- mysql/data/sys/session.frm
+++ mysql/data/sys/session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2021-03-11 22:56:33
+timestamp=2021-03-11 23:03:38
 create-version=1
 source=SELECT * FROM sys.processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
